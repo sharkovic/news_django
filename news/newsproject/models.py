a659a59f75e7cd5bed0c55b8d8d48fcd6cb2180d
sharkovic <1906Baikuzov6091>
2022-04-04 17:51:51 +0300

    с работы 1

--- news/newsproject/models.py
+++ news/newsproject/models.py
@@ -1,7 +1,38 @@
 from django.db import models
 
 
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField(default=0.0)
+
+
+director = 'DI'
+admin = 'AD'
+cook = 'CO'
+cashier = 'CA'
+cleaner = 'CL'
+
+POSITIONS = [
+    (director, 'Директор'),
+    (admin, 'Администратор'),
+    (cook, 'Повар'),
+    (cashier, 'Кассир'),
+    (cleaner, 'Уборщик')
+]
+
+
 class Staff(models.Model):
     full_name = models.CharField(max_length=255)
-    position = models.CharField(max_length=255)
+    position = models.CharField(max_length=2,
+                                choices=POSITIONS,
+                                default=cashier)
     labor_contact = models.IntegerField()
+
+
+class Order(models.Model):
+    time_in = models.DateTimeField(auto_now_add=True)
+    time_out = models.DateTimeField(null=True)
+    cost = models.FloatField(default=0.0)
+    pickup = models.BooleanField(default=False)
+    complete = models.BooleanField(default=False)
+    staff = models.ForeignKey(Staff, on_delete=models.CASCADE)
