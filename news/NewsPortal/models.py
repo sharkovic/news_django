409ce1ac42311575c3a42396cdff33c7af143fed
sharkovic <sharkghost@mail.ru>
2022-04-14 20:52:53 +0300

    v3

--- news/NewsPortal/models.py
+++ news/NewsPortal/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from datetime import datetime
+import datetime
 from django.db.models import Sum
 
 
--- news/NewsPortal/templatetags/custom_filters.py
+++ news/NewsPortal/templatetags/custom_filters.py
@@ -11,5 +11,5 @@ def censor(value):
     wrong_words = ['судорога', 'пельмень', 'балаклава', 'кочерыжка', 'родственник']
     for i in wrong_words:
         if i.find(value):
-            value = value.replace(i[1::], "*" * len(i))
+            value = value.replace(i[1::], "*" * (len(i)-1))
     return f'{value}'
